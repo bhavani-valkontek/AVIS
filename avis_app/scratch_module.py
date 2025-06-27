5bb7a1f8f1b858000693b175090cdfd2b0f03fbe
bhavani-valkontek <bhavani.babbili@valkontek.com>
2025-06-27 16:54:02 +0530

    Update scratch_module.py

--- avis_app/scratch_module.py
+++ avis_app/scratch_module.py
@@ -108,7 +108,7 @@ def load_model():
     if not os.path.exists(model_path):
 
         try:
-            hf_token = ""  # 🔐 Replace with your real token or use secrets
+            hf_token = st.secrets["gcp_service_account"]["Apikey"]  # 🔐 Replace with your real token or use secrets
             headers = {"Authorization": f"Bearer {hf_token}"}
             response = requests.get(hf_url, headers=headers)
 
