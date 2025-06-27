e284c4a60349c4906d052e30e2e26dc816c068db
bhavani-valkontek <bhavani.babbili@valkontek.com>
2025-06-27 17:57:15 +0530

    Update scratch_module.py

--- avis_app/scratch_module.py
+++ avis_app/scratch_module.py
@@ -24,17 +24,17 @@ from pytz import timezone
 def load_drive():
     # Load service account info from Streamlit secrets
     creds_dict = {
-        "type": st.secrets["gcp_service_account"]["type"],
-        "project_id": st.secrets["gcp_service_account"]["project_id"],
-        "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-        "private_key": st.secrets["gcp_service_account"]["private_key"],
-        "client_email": st.secrets["gcp_service_account"]["client_email"],
-        "client_id": st.secrets["gcp_service_account"]["client_id"],
-        "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-        "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
-        "universe_domain": st.secrets["gcp_service_account"]["universe_domain"]
+        "type": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["type"],
+        "project_id": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["project_id"],
+        "private_key_id": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["private_key_id"],
+        "private_key": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["private_key"],
+        "client_email": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["client_email"],
+        "client_id": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["client_id"],
+        "auth_uri": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["auth_uri"],
+        "token_uri": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["GDRIVE_SERVICE_ACCOUNT"]["universe_domain"]
     }
 
 
